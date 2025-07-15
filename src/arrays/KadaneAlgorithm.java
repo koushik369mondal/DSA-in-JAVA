507f40a7649f7ece9ea3a4e1e515675110f55b99
Kaushik Mandal <koushik369mondal@gmail.com>
2025-07-16 01:55:22 +0530

    KadaneAlgorithm program

--- src/arrays/KadaneAlgorithm.java
+++ src/arrays/KadaneAlgorithm.java
@@ -1,5 +1,22 @@
 public class KadaneAlgorithm {
     public static void main(String[] args) {
-        
+        int[] arr = { -2, -3, 4, -1, -2, 1, 5, -3 };
+
+        int currentSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            currentSum += arr[i];
+
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+
+            if (currentSum < 0) {
+                currentSum = 0; // reset if sum goes negative
+            }
+        }
+
+        System.out.println("Maximum Subarray Sum: " + maxSum);
     }
 }
