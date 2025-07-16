42a17ac9003a8639a88d1c997f2e8024c0f50d6a
Kaushik Mandal <koushik369mondal@gmail.com>
2025-07-17 00:39:59 +0530

    Contains Duplicate HashMap program

---
+++ src/arrays/easy/ContainsDuplicateHashMap.java
@@ -0,0 +1,28 @@
+package easy;
+
+import java.util.*;
+
+class ContainsDuplicateHashMap {
+    public static boolean containsDuplicate(char[] nums) {
+        int n = nums.length;
+        HashMap<Character, Integer> map = new HashMap<>(); // Use Character, not Integer
+
+        for (int i = 0; i < n; i++) {
+            // Update frequency count
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+
+            // If count > 1, it's a duplicate
+            if (map.get(nums[i]) > 1) {
+                return true;
+            }
+        }
+
+        return false; // No duplicates found
+    }
+
+    public static void main(String[] args) {
+        char[] nums = { 'a', 'b', 'c', 'd' };
+        System.out.println("Contains Duplicate: " + containsDuplicate(nums));
+    }
+}
+
