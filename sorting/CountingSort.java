a267f56220b95d796497a07083b360244964e2a1
Kaushik Mandal <koushik369mondal@gmail.com>
2025-08-03 20:31:40 +0530

    CountingSort implementation

---
+++ sorting/CountingSort.java
@@ -0,0 +1,37 @@
+public class CountingSort {
+    public static void countingSort(int arr[]) {
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+
+        int count[] = new int[largest + 1];
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+
+        // sorting the array
+        int j = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+
+    // Helper function to print array
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 4, 1, 2, 0, 3, 3, 2 };
+        countingSort(arr);
+        printArray(arr);
+    }
+}
