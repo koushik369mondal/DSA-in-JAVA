362c4b95db7c9194709b56aba88272afd4af8bdf
Kaushik Mandal <koushik369mondal@gmail.com>
2025-10-19 20:04:04 +0530

    Refactor diagonalSum method in SpiralMatrix class to simplify diagonal sum calculation and remove commented-out code

--- arrays/SpiralMatrix.java
+++ arrays/SpiralMatrix.java
@@ -37,19 +37,30 @@ public class SpiralMatrix {
         System.out.println();
     }
 
-    public static int diagonalSum(int matrix[][]){
+    public static int diagonalSum(int matrix[][]){ //0(N^2)
         int sum = 0;
 
+        // for(int i=0; i<matrix.length; i++){
+        //     for(int j=0; j<matrix[0].length; j++){
+        //         if(i == j){
+        //             sum += matrix[i][j];
+        //         }
+        //         else if(i + j == matrix.length-1) {
+        //             sum += matrix[i][j];
+        //         }
+        //     }
+        // }
+
         for(int i=0; i<matrix.length; i++){
-            for(int j=0; j<matrix[0].length; j++){
-                if(i == j){
-                    sum += matrix[i][j];
-                }
-                else if(i + j == matrix.length-1) {
-                    sum += matrix[i][j];
-                }
+            //primary diagonal
+            sum += matrix[i][i];
+
+            //secondary diagonal
+            if(i != matrix.length - 1 - i){ //to avoid adding the middle element twice in odd sized matrix
+                sum += matrix[i][matrix.length - 1 - i];
             }
         }
+
         return sum;
     }
     
