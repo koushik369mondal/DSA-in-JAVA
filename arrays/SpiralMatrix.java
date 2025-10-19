17ac7e4c563b5d18bd5b55fb63df6e925c852f6a
Kaushik Mandal <koushik369mondal@gmail.com>
2025-10-19 19:16:48 +0530

    Add diagonalSum method to SpiralMatrix class for calculating diagonal sums

--- arrays/SpiralMatrix.java
+++ arrays/SpiralMatrix.java
@@ -36,6 +36,22 @@ public class SpiralMatrix {
         }
         System.out.println();
     }
+
+    public static int diagonalSum(int matrix[][]){
+        int sum = 0;
+
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                if(i == j){
+                    sum += matrix[i][j];
+                }
+                else if(i + j == matrix.length-1) {
+                    sum += matrix[i][j];
+                }
+            }
+        }
+        return sum;
+    }
     
     public static void main(String[] args) {
         int matrix[][] = {
@@ -44,6 +60,6 @@ public class SpiralMatrix {
             {9, 10, 11, 12},
             {13, 14, 15, 16}
         };
-        printSpiral(matrix);
+        System.out.println(diagonalSum(matrix));
     }
 }
